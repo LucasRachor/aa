edf17582c785d8754ce9218b742f1add2a953586
Lucas Rachor <lucas.rachor32@gmail.com>
2023-11-14 19:31:44 -0400

    Fixed JwtAuth

--- src/auth/guards/JwtAuth.guard.ts
+++ src/auth/guards/JwtAuth.guard.ts
@@ -7,7 +7,7 @@ import {
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 import { IS_PUBLIC_KEY } from '../decorators/is-public.decorator';
-import { UnauthorizedError } from '../errors/unauthorizedError.error';
+import { UnauthorizedError } from '../errors/unauthorizederror.error';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
